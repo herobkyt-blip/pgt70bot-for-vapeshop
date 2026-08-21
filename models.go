97a42eaa105b8a09bc9699b9781a2e05fa55e00e
Maxim <herobkyt@gmail.com>
2026-08-21 10:46:02 +0700

    add test product list

--- models.go
+++ models.go
@@ -7,3 +7,9 @@ type Product struct {
 	Description string
 	InStock     bool
 }
+
+var products = []Product{
+	{ID: 1, Name: "Жидкость TRAVA", Price: 550, Description: "Жидкость для электронных сигарет TRAVA", InStock: true},
+	{ID: 2, Name: "Жидкость CHORME", Price: 540, Description: "Жидкость для электронных сигарет CHORME", InStock: true},
+	{ID: 3, Name: "Жидкость PODONKI", Price: 450, Description: "Жидкость для электронных сигарет PODONKI", InStock: true},
+}
