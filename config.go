e3ca0d87c95fdf7f185ec67ff15fc1a562a368b2
Maxim <herobkyt@gmail.com>
2026-08-21 13:18:47 +0700

    restore AdminIDs and IsAdmin check

--- config.go
+++ config.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	BotToken string
+	AdminIDs map[int64]bool
 }
 
 func LoadConfig() Config {
 	godotenv.Load()
+
+	adminIDs := map[int64]bool{}
+	idsRaw := os.Getenv("ADMIN_IDS")
+	for _, part := range strings.Split(idsRaw, ",") {
+		part = strings.TrimSpace(part)
+		if part == "" {
+			continue
+		}
+		if id, err := strconv.ParseInt(part, 10, 64); err == nil {
+			adminIDs[id] = true
+		}
+	}
+
 	return Config{
 		BotToken: os.Getenv("BOT_TOKEN"),
+		AdminIDs: adminIDs,
 	}
+}
+
+func (c Config) IsAdmin(userID int64) bool {
+	return c.AdminIDs[userID]
 }
